4a17e752ee194cf62eb1c09bd4cbab2dc9e499d0
Dayan <136735481+doocharbex@users.noreply.github.com>
2024-07-19 19:59:03 +0430

    [ Add Font Family ]
    Add Font Family 'Samim'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Samimfont']
+      },
+    },
   },
   plugins: [],
 }
